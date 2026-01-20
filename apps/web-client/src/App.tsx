0d150e116e933be2a4fb92b7543692a8b2256d75
Aaron Plahn <aplahn85@gmail.com>
2026-01-20 10:56:39 -0800

    scaffold the web client

---
+++ apps/web-client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='/denisiqi-logo-transparent.png' className="App-logo" alt="logo" />
+        True Impact
+      </header>
+    </div>
+  );
+}
+
+export default App;
