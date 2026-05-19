01af2f290c33d5fad4a78377483fb9532921784f
Aaron Plahn <aplahn85@gmail.com>
2026-05-19 14:48:33 -0700

    remove unused TS expect error directive

--- apps/server/src/features/survey/survey-events.gateway.ts
+++ apps/server/src/features/survey/survey-events.gateway.ts
@@ -110,7 +110,6 @@ const buildViewDiffForEvent = (e: BaseEvent): ViewDiff => {
 })
 export class SurveyEventsGateway implements OnGatewayConnection {
   @WebSocketServer()
-  // @ts-expect-error TS is unaware of the decorator's effect
   server: Server;
 
   /**
