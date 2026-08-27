6fceae326c286174b371d0a842696ad821b3c35e
dsstech <aplahn85@gmail.com>
2026-08-27 11:45:37 -0700

    track error state in survey report detail view

--- apps/web-client/src/components/surveys/review/survey-response-report-detail.page.tsx
+++ apps/web-client/src/components/surveys/review/survey-response-report-detail.page.tsx
@@ -1,6 +1,7 @@
 import { JSX, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { ApiResponseErrorInfo } from "../../error-handling";
 import { Loading } from "../../loading";
 import { useFetchSurveyByIdQuery } from "../store";
 import { makeSelectReportForSurveyResponseByResponseIdAndName } from "../store/survey-responses.api";
@@ -9,7 +10,9 @@ import { DefaultSurveyReportPresenter } from "./default-survey-report.presenter"
 export const SurveyResponseReportDetailPage = (): JSX.Element => {
   const { id: surveyResponseRecordId, reportName } = useParams();
 
-  useFetchSurveyByIdQuery(surveyResponseRecordId || "");
+  const { data, error, isLoading } = useFetchSurveyByIdQuery(
+    surveyResponseRecordId || "",
+  );
 
   const selectReport = useMemo(
     () =>
@@ -20,12 +23,20 @@ export const SurveyResponseReportDetailPage = (): JSX.Element => {
     [surveyResponseRecordId, reportName],
   );
 
-  // TODO what happened to the error state?
   const report = useSelector(selectReport);
+
+  if (isLoading || !data) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <ApiResponseErrorInfo error={error} />;
+  }
 
   if (!report) {
     return <Loading />;
   }
 
+  // TODO Support rich reports for specific reports by name (e.g., Medicine Wheel)
   return <DefaultSurveyReportPresenter report={report} />;
 };
