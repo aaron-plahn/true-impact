d4fbd40d9f1517329dd78d6fe3762a04a56bf206
dsstech <aplahn85@gmail.com>
2026-09-08 14:24:22 -0700

    WIP implement postgres event repository

--- apps/server/src/postgresql/postgres-event.repository.spec.ts
+++ apps/server/src/postgresql/postgres-event.repository.spec.ts
@@ -1,16 +1,26 @@
 import { Client, Pool } from 'pg';
-import { PostgresEventRepository } from './postgres-event.repository';
+import {
+  BaseEvent,
+  EventDocument,
+  IEventFactory,
+  PostgresEventRepository,
+} from './postgres-event.repository';
 
 const POSTGRES_HOST = 'localhost';
 const POSTGRES_PORT = 5432;
 const POSTGRES_USER = process.env.POSTGRES_USER;
 // TODO env var
+// DO THIS
 const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
-const POSTGRES_DB = process.env.POSTGRES_DB;
+const POSTGRES_DB = 'testpostgreseventrepository';
+
+if (!POSTGRES_DB) {
+  throw new Error(`missing var`);
+}
 
 const POSTGRES_CONNECTION_STRING = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
 
-const testDatabaseName = 'postgres-event-repository-test';
+const testDatabaseName = POSTGRES_DB;
 
 type ToyEventMeta = {
   userId: string;
@@ -20,6 +30,8 @@ type ToyEventMeta = {
 const WIDGET = 'WIDGET';
 
 class WidgetCreated {
+  streamId: string;
+
   readonly type = 'WIDGET_CREATED';
 
   readonly payload: {
@@ -28,9 +40,25 @@ class WidgetCreated {
   };
 
   readonly meta: ToyEventMeta;
+
+  revision: number;
+
+  constructor(doc: {
+    streamId: string;
+    type: 'WIDGET_CREATED';
+    payload: {
+      aggregateCompositeIdentifier: { id: string };
+      name: string;
+    };
+    meta: ToyEventMeta;
+  }) {
+    Object.assign(this, doc);
+  }
 }
 
 const firstWidgetCreated: WidgetCreated = {
+  streamId: 'widget123',
+  revision: 0,
   type: 'WIDGET_CREATED',
   payload: {
     aggregateCompositeIdentifier: {
@@ -45,6 +73,16 @@ const firstWidgetCreated: WidgetCreated = {
   },
 };
 
+const eventFactory: IEventFactory = {
+  build(eventDocument: EventDocument): BaseEvent {
+    if (eventDocument.event_type === 'WIDGET_CREATED') {
+      return new WidgetCreated(eventDocument as unknown as WidgetCreated);
+    }
+
+    throw new Error(`Unknown event type: ${eventDocument.event_type}`);
+  },
+};
+
 describe(`PostgresEventRepository`, () => {
   const pool = new Pool({
     connectionString: POSTGRES_CONNECTION_STRING,
@@ -55,34 +93,88 @@ describe(`PostgresEventRepository`, () => {
   let testRepository: PostgresEventRepository;
 
   beforeAll(async () => {
+    const adminClient = new Client({
+      host: POSTGRES_HOST,
+      port: POSTGRES_PORT,
+      user: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: 'postgres',
+      connectionTimeoutMillis: 1500,
+    });
 
+    await adminClient.connect();
+
+    const dbsWithNameQuery = `SELECT 1 from pg_catalog.pg_database WHERE datname = $1`;
+
+    const res = await adminClient
+      .query(dbsWithNameQuery, [testDatabaseName])
+      .catch(async (e) => {
+        await adminClient.end();
+
+        throw e;
+      });
+
+    if (res.rowCount === 0) {
+      // the database does not yet exist
+      // Note that CREATE DATABASE cannot be run in a parametrized query. It is important that the user is never able to inject the database name
+      // TODO can we sanitize \ validate the name just as an extra safe guard?
+      await adminClient.query(`CREATE DATABASE "${testDatabaseName}"`);
+    }
+
+    await adminClient.end();
+
+    // is this used?
     const client = new Client({
       host: POSTGRES_HOST,
       port: POSTGRES_PORT,
       user: POSTGRES_USER,
       password: POSTGRES_PASSWORD,
-      database: 'testdb',
+      database: testDatabaseName,
       connectionTimeoutMillis: 1500,
     });
 
     try {
       await client.connect();
 
-      console.log('searching 1');
-      const databaseSearchResult = await client.query(
-        `SELECT * FROM employees`,
-      );
-      console.log({ databaseSearchResult });
-      console.log('done 2');
-      if (databaseSearchResult.rowCount === 0) {
-        console.log('more queries 3');
-        // Should we use bind params here? We own the db name so there's not risk of an injection attack. Is that even possible with database names?
-        await client.query(`CREATE DATABASE $${testDatabaseName};`);
-      }
+      testRepository = new PostgresEventRepository(pool, eventFactory);
 
-      testRepository = new PostgresEventRepository(pool);
+      // stream_id, event_type, payload, metadata
+      // TODO test setup helper
+      // This would be terrible in production!
+
+      // TODO should we use truncate instead?
+      // Resets the table data instantly without deleting the table structure
+      // await pool.query('TRUNCATE TABLE users RESTART IDENTITY CASCADE;');
+
+      const dropTableIfExists = `
+      DROP TABLE IF EXISTS events;
+      `;
+
+      await client.query(dropTableIfExists).catch((e) => {
+        throw e;
+      });
+
+      // TODO use a lib to get static analysis \ type safety on SQL queries
+      // not null constraints?
+      const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS events (
+        stream_id TEXT PRIMARY KEY,
+        revision INT NOT NULL,
+        event_type VARCHAR(64) NOT NULL,
+        payload JSONB DEFAULT '{}'::jsonb,
+        metadata JSONB DEFAULT '{}'::jsonb,
+        CONSTRAINT uq_stream_version UNIQUE (stream_id, revision)
+        );
+      `;
+
+      await client.query(createTableQuery).catch((e) => {
+        throw e;
+      });
 
       await client.end();
+
+      console.log('done');
+      // todo after all ``client.end()
     } catch (error) {
       await client.end();
 
@@ -96,11 +188,15 @@ describe(`PostgresEventRepository`, () => {
 
   describe(`when creating a first event`, () => {
     it(`should persist the event`, async () => {
-      await testRepository.appendEvent(firstWidgetCreated);
+      await testRepository.appendEvent(firstWidgetCreated, 0);
 
-      const searchResult = await testRepository.read({ type: WIDGET, id: '1' });
+      const searchResult = await testRepository.read();
 
       expect(searchResult).toHaveLength(1);
+
+      const foundRecord = searchResult[0];
+
+      expect(foundRecord).toBeInstanceOf(WidgetCreated);
 
       // TODO Check that all props are persisted properly
     });
--- apps/server/src/postgresql/postgres-event.repository.ts
+++ apps/server/src/postgresql/postgres-event.repository.ts
@@ -1,17 +1,40 @@
+import { NotImplementedException } from '@nestjs/common';
 import { Pool } from 'pg';
 import { Inject } from '../libs/framework';
 import { PG_POOL_INJECTION_TOKEN } from './postgres.module';
 
-interface IEvent {
-  type: string;
+export interface EventDocument {
+  event_type: string;
+  streamId: string;
   payload: Record<string, unknown>;
   meta: Record<string, unknown>;
+}
+
+export interface EventPayload {
+  aggregateCompositeIdentifier: {
+    type: string;
+    id: string;
+  };
+}
+
+export interface BaseEvent<T extends EventPayload = EventPayload> {
+  type: string;
+  streamId: string;
+  payload: T;
+  meta: Record<string, unknown>;
+  revision: number;
+}
+
+export interface IEventFactory {
+  build(eventDocument: EventDocument): BaseEvent;
 }
 
 export class PostgresEventRepository {
   constructor(
     @Inject(PG_POOL_INJECTION_TOKEN)
     private readonly pool: Pool,
+    @Inject('EVENT_FACTORY_INJECTION_TOKEN')
+    private readonly eventFactory: IEventFactory,
   ) {}
 
   // couldn't this potentially return an error?
@@ -20,17 +43,31 @@ export class PostgresEventRepository {
    * The idea would be that we project off n streams and then report the streams we used
    * when committing and fail an optimistic concurrency check if any of those streams has been edited.
    */
-  async appendEvent(_event: IEvent): Promise<{ streamId: string } | Error> {
+  async appendEvent(
+    event: BaseEvent,
+    // necessary for optimistic concurrency
+    revision: number,
+  ): Promise<{ streamId: string } | Error> {
+    await this.pool.query('BEGIN');
+
     // stream_version?
     const query = `
-        INSERT INTO events (stream_id, event_type, payload, metadata)
-        VALUES ( $1, $2, $3, $4)
-        ON CONFLICT (stream_id) DO NOTHING;
+        INSERT INTO events (stream_id, event_type, payload, metadata, revision)
+        VALUES ($1, $2, $3, $4, $5 + 1)
+        ON CONFLICT (stream_id, revision) DO NOTHING;
     `;
 
-    const values = [];
+    const values = [
+      event.streamId,
+      event.type,
+      event.payload,
+      event.meta,
+      revision,
+    ];
 
-    const result = await this.pool.query(query, values);
+    const result = await this.pool.query(query, values).catch((_e) => {
+      throw new Error(`TODO MAke this a returned error`);
+    });
 
     if (result.rowCount === 0) {
       return new Error(
@@ -49,7 +86,31 @@ export class PostgresEventRepository {
   async read(_aggregateCompositeIdentifier?: {
     type: string;
     id: string;
-  }): Promise<IEvent[]> {
-    return Promise.resolve([]);
+  }): Promise<BaseEvent[]> {
+    if (_aggregateCompositeIdentifier) {
+      throw new NotImplementedException(`Event filters are not yet supported`);
+    }
+
+    const selectAllEvents = `SELECT * FROM events`;
+
+    const rawRows = await this.pool
+      .query<EventDocument>(selectAllEvents)
+      .catch((e) => {
+        // TODO return errors
+        throw e;
+      });
+
+    /**
+     * It is the repository's responsibility to hydrate an instance of an event. This means that we need an `EventFactory`.
+     * The event factory can be built by dynamically registering events `@DomainEvent` and using a plugin style architecture.
+     * ```ts
+     * myEventFactory.register("MY_EVENT",()=> MyEvent.fromDocument(eventDoc))
+     *
+     * We should be careful around the design of this. If it's possible to avoid running discovery up front, that might be ideal.
+     * We could consider injecting the factory into the read method instead of the constructor. We could consider injecting a way to lazily lookup the meta for
+     * the factory when needed instead of eagerly building the entire factory. There are lots of things to think about here.
+     * ```
+     */
+    return rawRows.rows.map((row) => this.eventFactory.build(row));
   }
 }
